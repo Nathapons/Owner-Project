7156af67510a798436af8826e8285fe9a3af68f8
Nathapon.S <nathapon.s@th.fujikura.com>
2021-07-01 08:58:07 +0700

    commit

--- DataMergeVisual.py
+++ DataMergeVisual.py
@@ -1,45 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+from tkcalendar import Calendar
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import statistics
 
-csv_file = "D:\\Nathapon_KeepFolder\\0.My work\\01.IoT\\08.P'Muse\\SERVER\\20210627\\RESULT\\ACT_RESULT.CSV"
-df = pd.read_csv(csv_file)
+class DataMergeVisual():
+    def __init__(self):
+        root = Tk()
+        WIDTH = 500
+        HEIGHT = 300
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
 
-datetime_lists = []
-avg1_lists = []
-tooltips = []
-max_row = list(df.shape)[0]
+        # Root Properties
+        label = pd.read
+        root.mainloop()
 
-count = 0
-for row in range(max_row):
-    avg1 = float(df['CPU System'][row])
-    date = str(df['Date'][row])
-    time = str(df['Time'][row])
-    datetime = date + " " + time
-    tooltip = "Date: " + datetime + " Values: " + str(avg1)
 
-    avg1_lists.append(avg1)
-    datetime_lists.append(count)
-    tooltips.append(tooltip)
-
-    count += 1
-    # if count >= 10:
-    #     break
-
-# Plot Graph
-plt.plot(datetime_lists, avg1_lists, linestyle='-', marker='.', color='black')
-mean = statistics.mean(avg1_lists)
-plt.axhline(mean, color='green')
-
-# Graph view Setting
-max_val = int(max(avg1_lists))
-min_val = int(min(avg1_lists))
-ymarg = (max_val - min_val) * plt.margins()[1]
-plt.yticks(np.arange(min_val, max_val, 2))
-title_setting = {'size':20,'color':'blue'}
-plt.ylabel('Load Average 1m')
-plt.xlabel('Datetime', rotation=0)
-plt.title('Load Averge 1m', title_setting)
-
-plt.show()
+if __name__ == '__main__':
+    app = DataMergeVisual()
